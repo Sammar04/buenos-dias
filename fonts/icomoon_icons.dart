f339dc3fe2ce3c7925266834dd5c6ac25b8b0d01
Sammar04 <83387090+Sammar04@users.noreply.github.com>
2021-04-29 01:28:05 -0600

    Add files via upload

---
+++ fonts/icomoon_icons.dart
@@ -0,0 +1,22 @@
+// Place fonts/icomoon.ttf in your fonts/ directory and
+// add the following to your pubspec.yaml
+// flutter:
+//   fonts:
+//    - family: icomoon
+//      fonts:
+//       - asset: fonts/icomoon.ttf
+import 'package:flutter/widgets.dart';
+
+class Icomoon {
+  Icomoon._();
+
+  static const String _fontFamily = 'icomoon';
+
+  static const IconData controller_fast_backward = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData home = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData heart = IconData(0xe9da, fontFamily: _fontFamily);
+  static const IconData tongue2 = IconData(0xe9e4, fontFamily: _fontFamily);
+  static const IconData wink2 = IconData(0xe9e8, fontFamily: _fontFamily);
+  static const IconData cool2 = IconData(0xe9ec, fontFamily: _fontFamily);
+  static const IconData hipster2 = IconData(0xe9fa, fontFamily: _fontFamily);
+}
